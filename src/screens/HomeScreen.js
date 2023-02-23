56b81a21ae8c28fd5e12810a36f9a1f3f52072e1
Colin Bresnahan <colinbres12@gmail.com>
2023-02-22 16:38:00 -0800

    Added navigation buttons to home screen
    Added four buttons that take use to the PlaceBetScreen, LeaderBoardScreen, NewsScreen, and
    CurrentBetsScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React, {useLayoutEffect} from 'react';
-import {ScrollView, StyleSheet, Text} from 'react-native';
+import {ScrollView, View, StyleSheet, Text, TouchableHighlight} from 'react-native';
 import {connect, useSelector} from 'react-redux';
 import { AppStyles} from '../AppStyles';
 import {Configuration} from '../Configuration';
+import BasicButton from '../components/BasicButton';
 
 function HomeScreen({navigation}) {
   const auth = useSelector((state) => state.auth);
@@ -14,22 +15,42 @@ function HomeScreen({navigation}) {
   }, []);
 
   return (
-    <ScrollView style={styles.container}>
+    <View style={styles.container}>
       <Text style={styles.title}>Welcome {auth.user?.fullname ?? 'User'}</Text>
-    </ScrollView>
+      <BasicButton
+        onPress = {() => navigation.navigate('Betting Page')}
+        title = "Make Bets Here!"
+      />
+      <BasicButton
+        onPress = {() => navigation.navigate('Current Bets')}
+        title = "My Bets"
+      />
+      <BasicButton
+        onPress = {() => navigation.navigate('News')}
+        title = "News"
+      />
+      <BasicButton
+        onPress = {() => navigation.navigate('Leaderboard')}
+        title = "Leaderboard"
+      />
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: 'white',
+    margin: 0,
     flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
     padding: Configuration.home.listing_item.offset,
   },
   title: {
     fontWeight: 'bold',
     color: AppStyles.color.title,
     fontSize: 25,
+    textAlign: "center"
   },
   userPhoto: {
     width: 40,
