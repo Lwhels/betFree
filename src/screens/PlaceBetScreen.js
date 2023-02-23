c81bbc97cd2883cd14f60b1107c455267f1ef7dc
Lucas Wheeler <wheeler.lucas2002@gmail.com>
2023-02-23 00:38:33 -0800

    Integrated sports API
    from homescreen, store API data into global variables declared in
    global.js. Implemented new global variables into placebetscreen with
    limited functionality.
    TODO: Fix style of placebetscreen and fully flesh out functionality.

--- src/screens/PlaceBetScreen.js
+++ src/screens/PlaceBetScreen.js
@@ -1,43 +1,101 @@
 import React, {useLayoutEffect} from 'react';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from 'react-native';
 import {connect, useSelector} from 'react-redux';
-import { AppStyles} from '../AppStyles';
+import {AppStyles} from '../AppStyles';
 import {Configuration} from '../Configuration';
 
 
-export default function PlaceBetScreen({navigation}){
-    
-    useLayoutEffect(() => {
-        navigation.setOptions({
-          title: 'Betting Page',
-        });
-      }, []);
+export default function PlaceBetScreen({navigation}) {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: 'Betting Page',
+    });
+  }, []);
+  // <Image source={{uri: names[1]}} style={{width: 100, height: 100}} />
+  /*
+  var buttonsListArr = [];
 
-    return (
-        <View style={styles.container}>
-          <Text style={styles.title}> Betting Page </Text>
-        </View>
-      );
+ for (let i = 0; i < global.fetched_odds.length; i++) {
+    var game = global.fetched_odds[i]['game'];
+    var teams = game['teams'];
+    var score = game['scores'];
+    var timeRemaining = game['status']['long'];
+    var home = teams['home'];
+    var away = teams['away'];
+
+    buttonsListArr.push(
+      <TouchableHighlight style={styles.btnStyle}>
+        <Image source={{uri: home['logo']}} style={{width: 40, height: 40}} />
+        <Text> {home['name']} </Text>
+      </TouchableHighlight>
+    );
+  }
+*/
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> Betting Page </Text>
+      <FlatList
+        data={global.fetched_odds}
+        renderItem={({item}) => (
+          <View>
+            <View>
+            <TouchableOpacity>
+            <Text>
+            <Image source={{uri: item['game']['teams']['home']['logo']}} style={{width: 20, height: 20}}/>
+            <Text>                                                                   </Text>
+            <Image source={{uri: item['game']['teams']['away']['logo']}} style={{width: 20, height: 20}}/>
+            </Text>
+            <Text>
+            <Text>{item['game']['teams']['home']['name']}</Text>
+            <Text> Vs. </Text>
+            <Text>{item['game']['teams']['away']['name']} </Text>
+            </Text>
+            </TouchableOpacity>
+            <Text> {'\n'}</Text>
+            </View>
+          </View>
+        )}
+        keyExtractor={(item) => item['game']['id']}
+        style= {styles.list}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    margin: 0,
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    padding: Configuration.home.listing_item.offset,
   },
   title: {
     fontWeight: 'bold',
     color: AppStyles.color.title,
     fontSize: 25,
-    textAlign: "center"
+    textAlign: 'center',
   },
   userPhoto: {
     width: 40,
     height: 40,
     borderRadius: 20,
     marginLeft: 5,
+  },
+  btnStyle: {
+    justifyContent: 'center',
+    width: '85%',
+    height: '8%',
+    marginTop: 30,
+    borderRadius: 15,
+    backgroundColor: 'green',
+  },
+  list: {
+    width: '85%',
   },
 });
