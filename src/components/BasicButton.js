8b9658711e3f2c0ac047155bf749a46bd9858c66
Colin Bresnahan <colinbres12@gmail.com>
2023-02-22 16:39:41 -0800

    Slight modificaiton to BasicButton sytling
    More padding, taller, and wider button.

--- src/components/BasicButton.js
+++ src/components/BasicButton.js
@@ -3,7 +3,6 @@ import {StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 
 export default function BasicButton(props){
     return(
-        //<View style={styles.centered}>
          <TouchableHighlight
             style={styles.btnStyle}
             onPress={props.onPress}>
@@ -11,9 +10,6 @@ export default function BasicButton(props){
                 <Text style={styles.btnTxt}>{props.title}</Text>
             </View>
        </TouchableHighlight> 
-      // </View>
-           
-
     )
 }
 
@@ -25,10 +21,10 @@ const styles = StyleSheet.create({
     },
     btnStyle:{
         justifyContent: 'center',
-        width: '82%',
+        width: '85%',
         height: '8%',
-        marginTop: 20,
-        borderRadius: 10,
+        marginTop: 30,
+        borderRadius: 15,
         backgroundColor: 'green',
     },
     btnTxt:{
