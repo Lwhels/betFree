db07aca40161bcc86bc896360738bc2c61395758
Kaylee Tran <kayleetran@Kaylees-Macbook-Pro.local>
2023-03-04 23:05:14 -0800

    profile frontend with hardcoded values, hideable password

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -2,29 +2,110 @@ import React, {useEffect, useState} from 'react';
 import auth from '@react-native-firebase/auth';
 import firebase from '@react-native-firebase/app';
 import firestore from '@react-native-firebase/firestore';
-import {ScrollView, StyleSheet, Text, View} from 'react-native';
-import {AppStyles} from '../AppStyles'; 
+import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+import {AppIcon, AppStyles} from '../AppStyles'; 
 import { Image } from 'react-native';
 
+
+
 function ProfileScreen({navigation}) {
+  const balance = 100;
+  const [showPass, setShowPass] = useState (true); 
+  const password =  'kayleetran';
+
+  const showPassword = () => {
+    setShowPass(!showPass); 
+  }
+
   return (
     <View >
-      <Image> </Image>
+      <Image style = {styles.userImg} source={AppIcon.images.basketball}/> 
+      <Text style = {styles.username}> username </Text>
+      <Text style = {styles.balance}> Your balance: ${balance}</Text>
+      <Text style = {styles.fieldTitles}> name </Text>
+      <Text style = {styles.fields} > name </Text>
+      <Text style = {styles.fieldTitles}> email </Text>
+      <Text style = {styles.fields} > email </Text>
+      <Text style = {styles.fieldTitles}> password </Text>
+      <View style = {styles.container}>
+      <TextInput style = {styles.password} value = {password} secureTextEntry = {showPass}></TextInput>
+      <TouchableOpacity onPress = {showPassword}>
+        <Image 
+          style = {styles.showPasswordButton}
+          source = {showPass? require('../../assets/icons/openeye.png') : require('../../assets/icons/closeeye.png') }></Image>
+      </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  title: {
-    fontSize: AppStyles.fontSize.title,
+  username: {
+    fontSize: 20,
     fontWeight: 'bold',
-    color: AppStyles.color.tint,
-    marginTop: 20,
+    color: '#000000',
+    marginTop: 10,
     textAlign: 'center',
     marginBottom: 20,
     marginLeft: 20,
     marginRight: 20,
   },
+  userImg: {
+      borderColor: '#e6e6e6',
+      borderRadius: 85, 
+      borderWidth: 1, 
+      height: 170, 
+      width: 170, 
+      marginBottom: 10,
+      marginTop: 30, 
+      alignSelf: 'center',
+  },
+  balance: {
+    fontSize: 25,
+    fontWeight: 'bold',
+    color: '#1d6e1d',
+    marginTop: 10,
+    textAlign: 'center',
+    marginBottom: 20,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  fieldTitles: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#000000',
+    marginTop: 10,
+    textAlign: 'left',
+    marginBottom: 15,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  fields: {
+    fontSize: 18,
+    color: '#424242',
+    textAlign: 'left',
+    marginBottom: 20,
+    marginLeft: 30,
+    marginRight: 20,
+  },
+  password: {
+    fontSize: 18,
+    color: '#424242',
+    textAlign: 'left',
+    marginBottom: 20,
+    marginLeft: 35,
+    marginRight: 20,
+    width: '65%',
+  },
+  showPasswordButton: {
+    height: 30, 
+    width: 30, 
+    marginBottom: 20,
+    marginLeft: '20%',
+  },
+  container: {
+    flexDirection: 'row',
+  }
 });
 
 export default ProfileScreen;
