f23b3bd87403bd52a25c6798a61f6d8ce3fbe595
joeyj11 <joey11dev@gmail.com>
2023-03-05 13:47:57 -0800

    Implemented leaderboard backend
    Utilized orderBy() method to implement backend calls to create
    leaders for balance and correct bets placed.
    Implemented temporary console log leaderboard that displays
    these leaders in descending order.

--- src/screens/LeaderboardScreen.js
+++ src/screens/LeaderboardScreen.js
@@ -3,11 +3,62 @@ import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import {connect, useSelector} from 'react-redux';
 import { AppStyles} from '../AppStyles';
 import {Configuration} from '../Configuration';
+import firestore from '@react-native-firebase/firestore';
+import 'firebase/compat/firestore';
 
+/* ///////////////////////////////////////////   CREATE LEADERBOARD BACKEND   //////////////////////////////////////////////// */
+
+var balanceLeaders = [];
+var betWinLeaders = [];
+
+async function createBetWinLeaders(){
+  await firestore().collection("users").orderBy('betWins', 'desc').limit(20).get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        var data = doc.data();
+        // doc.data() is never undefined for query doc snapshots
+        betWinLeaders.push({
+          betWins: data.betWins,
+          name: data.fullname,
+        });
+        //console.log(doc.balance, " => ", doc.data());
+      });
+    });
+    // [END get_multiple_all]
+}
+
+async function createBalanceLeaders(){
+  await firestore().collection("users").orderBy('balance', 'desc').limit(20).get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        var data = doc.data();
+        // doc.data() is never undefined for query doc snapshots
+        balanceLeaders.push({
+          balance: data.balance,
+          name: data.fullname,
+        });
+        //console.log(doc.balance, " => ", doc.data());
+      });
+    });
+    // [END get_multiple_all]
+}
+
+async function leaderboard(){
+  await createBalanceLeaders();
+  for (let i = 0; i < balanceLeaders.length; i++){
+    console.log('Rank ' + (i + 1) + ' in balance: ' + balanceLeaders[i].name + ' with $' + balanceLeaders[i].balance);
+  }
+  console.log('\n\n');
+  await createBetWinLeaders();
+  for (let i = 0; i < betWinLeaders.length; i++){
+    console.log('Rank ' + (i + 1) + ' in correct bets: ' + betWinLeaders[i].name + ' with ' + betWinLeaders[i].betWins + ' correct bets.');
+  }
+}
+
+/* ///////////////////////////////////////////   END CREATE LEADERBOARD BACKEND   //////////////////////////////////////////////// */
 
 export default function LeaderboardScreen({navigation}){
     
     useLayoutEffect(() => {
+        leaderboard();
         navigation.setOptions({
           title: 'Leaderboard',
         });
