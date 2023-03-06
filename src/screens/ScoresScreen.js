9f02d503f2708906746c1756162f155229e0d608
Lucas Wheeler <wheeler.lucas2002@gmail.com>
2023-03-06 12:26:42 -0800

    Added filters to scores page
    added filters to scores page that enable users to change which nba
    scores they see. Users can filter by team or date

--- src/screens/ScoresScreen.js
+++ src/screens/ScoresScreen.js
@@ -8,10 +8,16 @@ import {
   Image,
   ActivityIndicator,
   RefreshControl,
+  Modal,
+  TouchableOpacity,
+  SafeAreaView,
+  Dimensions,
 } from 'react-native';
 import {connect, useSelector} from 'react-redux';
 import {AppStyles} from '../AppStyles';
 import {Configuration} from '../Configuration';
+import SelectDropdown from 'react-native-select-dropdown';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import '../global.js';
 
 function gameStatus(item) {
@@ -34,9 +40,57 @@ function gameStatus(item) {
   }
   return ret;
 }
+
 export default function ScoresScreen({navigation}) {
+  const teams = [
+    'All',
+    'Atlanta Hawks',
+    'Boston Celtics',
+    'Brooklyn Nets',
+    'Charlotte Hornets',
+    'Chicago Bulls',
+    'Cleveland Cavaliers',
+    'Dallas Mavericks',
+    'Denver Nuggets',
+    'Detroit Pistons',
+    'Golden State Warriors',
+    'Houston Rockets',
+    'Indiana Pacers',
+    'Los Angeles Clippers',
+    'Los Angeles Lakers',
+    'Memphis Grizzlies',
+    'Miami Heat',
+    'Milwaukee Bucks',
+    'Minnesota Timberwolves',
+    'New Orleans Pelicans',
+    'New York Knicks',
+    'Oklahoma City Thunder',
+    'Orlando Magic',
+    'Philadelphia Sixers',
+    'Phoenix Suns',
+    'Portland Trail Blazers',
+    'Sacramento Kings',
+    'San Antonio Spurs',
+    'Toronto Raptors',
+    'Utah Jazz',
+    'Washington Wizards',
+  ];
   const [loading, setLoading] = useState(true);
   const [refreshing, setRefreshing] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [filters, setFilters] = useState([]);
+  const [team, setTeam] = useState(teams[0]);
+  const [date, setDate] = useState(new Date());
+  const [show, setShow] = useState(false);
+  const [dateVisible, setDateVisible] = useState(false);
+
+  const showMode = (currentMode) => {
+    if (Platform.OS === 'android') {
+      setShow(false);
+      // for iOS, add a button that closes the picker
+    }
+    setMode(currentMode);
+  };
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -68,23 +122,55 @@ export default function ScoresScreen({navigation}) {
     setLoading(false);
     setRefreshing(false);
   };
+  function dateComp(apiDate, filterDate, filterMonth, filterYear) {
+    var apiYear = Number(apiDate.substring(0, 4));
+    var apiMonth = Number(apiDate.substring(5, 7));
+    var apiDay = Number(apiDate.substring(8, 10));
+    filterMonth = filterMonth + 1;
+    filterDate = filterDate + 1;
+
+    if (apiYear < filterYear) {
+      return true;
+    }
+    if (apiYear == filterYear) {
+      if (apiMonth < filterMonth) {
+        return true;
+      }
+      if (apiMonth == filterMonth) {
+        if (apiDay <= filterDate) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+  var filterYear = date.getFullYear();
+  var filterMonth = date.getMonth();
+  var filterDate = date.getDate();
   var allGames = [];
   var allGames = games;
-  console.log(typeof allGames);
-  console.log(typeof games);
-  allGames.reverse();
   var gamesToDisplay = [];
-
   for (let i = 0; i < allGames.length; i++) {
     if (
       allGames[i]['status']['short'] != 'NS' &&
       allGames[i]['status']['short'] != 'CANC'
     ) {
-      gamesToDisplay.push(allGames[i]);
+      if (team != 'All') {
+        if (
+          team != allGames[i]['teams']['home']['name'] &&
+          team != allGames[i]['teams']['away']['name']
+        ) {
+          continue;
+        }
+      }
+      if (dateComp(allGames[i]['date'], filterDate, filterMonth, filterYear)) {
+        gamesToDisplay.push(allGames[i]);
+      } else {
+        break;
+      }
     }
   }
-  gamesToDisplay = gamesToDisplay.splice(0, 50);
-
+  gamesToDisplay.reverse();
   function displayDate(date) {
     var localDate = new Date(date);
     localDate = String(localDate);
@@ -95,7 +181,20 @@ export default function ScoresScreen({navigation}) {
   function homeWin(game) {
     return game['scores']['home']['total'] > game['scores']['away']['total'];
   }
-  allGames.reverse();
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate;
+    setDateVisible(false);
+    setDate(currentDate);
+  };
+  function closeModal() {
+    setModalVisible(!modalVisible);
+  }
+  function openFilters() {
+    setModalVisible(!modalVisible);
+    //setDate(date);
+    //setTeam(teams[0]);
+  }
+
   if (loading) {
     return (
       <ActivityIndicator
@@ -108,15 +207,81 @@ export default function ScoresScreen({navigation}) {
   } else {
     return (
       <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert('Modal has been closed.');
+            setModalVisible(!modalVisible);
+          }}>
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text>Team:</Text>
+              <SelectDropdown
+                data={teams}
+                onSelect={(selectedItem, index) => {
+                  setTeam(selectedItem);
+                }}
+                buttonTextAfterSelection={(selectedItem, index) => {
+                  // text represented after item is selected
+                  // if data array is an array of objects then return selectedItem.property to render after item is selected
+                  return selectedItem;
+                }}
+                rowTextForSelection={(item, index) => {
+                  // text represented for each item in dropdown
+                  // if data array is an array of objects then return item.property to represent item in dropdown
+                  return item;
+                }}
+                defaultValue={team}
+              />
+
+              <Text>Date:</Text>
+              <TouchableOpacity
+                onPress={() => setDateVisible(true)}
+                style={styles.matchSelectDropdownButton}>
+                <Text style={styles.matchSelectDropdownText}>
+                  Up Until:{[' ', date.toString().substring(0, 16)]}
+                </Text>
+              </TouchableOpacity>
+
+              <Modal
+                visible={dateVisible}
+                animationType="slide"
+                transparent={true}
+                onRequestClose={() => {
+                  Alert.alert('Modal has been closed.');
+                  setDateVisible(!dateVisible);
+                }}>
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode="date"
+                  onChange={onChange}
+                />
+              </Modal>
+
+              <TouchableOpacity
+                style={[styles.button, styles.buttonOpen]}
+                onPress={() => closeModal()}>
+                <Text style={styles.textStyle}>close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
         <Text style={styles.title}> Scores Page </Text>
+        <TouchableOpacity
+          style={[styles.button, styles.buttonFilter, styles.listSpacing]}
+          onPress={() => openFilters()}>
+          <Text>Filters</Text>
+        </TouchableOpacity>
         <FlatList
           refreshControl={
             <RefreshControl refreshing={refreshing} onRefresh={fetchGames} />
           }
           data={gamesToDisplay}
           renderItem={({item}) => (
-            <View style={styles.flexCol}>
-              <Text> {'\n'}</Text>
+            <View style={[styles.flexCol, styles.listSpacing]}>
               <View style={styles.outerView}>
                 <View style={styles.flexCol}>
                   <View style={styles.logoTeam}>
@@ -171,6 +336,7 @@ export default function ScoresScreen({navigation}) {
   }
 }
 
+const {width} = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     margin: 0,
@@ -245,5 +411,73 @@ const styles = StyleSheet.create({
     height: 0,
     backgroundColor: 'transparent',
     borderStyle: 'solid',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'black',
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  buttonFilter: {
+    backgroundColor: 'tomato',
+  },
+  listSpacing: {
+    marginTop: '3%',
+    marginBottom: '3%',
+  },
+  datePickerStyle: {
+    width: 200,
+    marginTop: 20,
+  },
+  matchSelectDropdownButton: {
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#EFEFEF',
+    width: width / 2,
+    height: 50,
+    paddingHorizontal: 8,
+    overflow: 'hidden',
+  },
+  matchSelectDropdownText: {
+    flex: 1,
+    fontSize: 18,
+    color: '#000000',
+    textAlign: 'center',
+    marginHorizontal: 8,
   },
 });
