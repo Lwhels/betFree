f48deb4c4c8f0425a6178bee11e816bf21957a80
Colin Bresnahan <colinbres12@gmail.com>
2023-03-11 14:54:16 -0800

    Adding a new user interface scores screen
    The new UI uses flex rows with flexbasis, flexgrow, and flexshrink to make it so that the logos,
    names, scores, and the date are centered.

--- src/screens/ScoresScreen.js
+++ src/screens/ScoresScreen.js
@@ -247,10 +247,9 @@ export default function ScoresScreen({navigation}) {
 
               <Modal
                 visible={dateVisible}
-                animationType="slide"
                 transparent={true}
                 onRequestClose={() => {
-                  Alert.alert('Modal has been closed.');
+                  Alert.alert('Modsal has been closed.');
                   setDateVisible(!dateVisible);
                 }}>
                 <DateTimePicker
@@ -258,6 +257,7 @@ export default function ScoresScreen({navigation}) {
                   value={date}
                   mode="date"
                   onChange={onChange}
+                  style={{width: 320, backgroundColor: "white"}}
                 />
               </Modal>
 
@@ -281,69 +281,133 @@ export default function ScoresScreen({navigation}) {
           }
           data={gamesToDisplay}
           renderItem={({item}) => (
-            <View style={[styles.flexCol, styles.listSpacing]}>
-              <View style={styles.outerView}>
-                <View style={styles.flexCol}>
-                  <View style={styles.logoTeam}>
-                    <Image
-                      source={{uri: item['teams']['away']['logo']}}
-                      style={styles.userPhoto}
-                    />
-                    <Text style={styles.body}>
-                      {item['teams']['away']['name']}{' '}
-                    </Text>
-                    <Text style={styles.flexRight}>
-                      {' '}
-                      {item['scores']['away']['total']}
-                    </Text>
-                    {!homeWin(item) &&
-                    (item['status']['short'] == 'FT' ||
-                      item['status']['short'] == 'AOT') ? (
-                      <View style={[styles.triangle, styles.arrowLeft]} />
-                    ) : null}
-                  </View>
-                  <View style={styles.logoTeam}>
-                    <Image
-                      source={{uri: item['teams']['home']['logo']}}
-                      style={styles.userPhoto}
-                    />
-                    <Text style={styles.body}>
-                      {item['teams']['home']['name']}{' '}
-                    </Text>
-                    <Text style={styles.flexRight}>
-                      {item['scores']['home']['total']}
-                    </Text>
-                    {homeWin(item) &&
-                    (item['status']['short'] == 'FT' ||
-                      item['status']['short'] == 'AOT') ? (
-                      <View style={[styles.triangle, styles.arrowLeft]} />
-                    ) : null}
-                  </View>
-                </View>
-                <View style={styles.verticleLine}></View>
-                <View style={styles.dateStatus}>
-                  <Text>{gameStatus(item)}</Text>
-                  <Text>{displayDate(item['date'])}</Text>
-                </View>
-              </View>
-            </View>
-          )}
-          keyExtractor={(item) => item['id']}
-          style={styles.list}
-        />
+            <View style={styles.previewContainer}>
+        <View
+          style={[
+           styles.box,
+            {
+              flexBasis: 55,
+              flexGrow: 0,
+              flexShrink: 1,
+              //backgroundColor: 'powderblue',
+              justifyContent: "space-around",
+            },
+          ]}>
+          <Image
+                    source={{uri: item['teams']['away']['logo']}}
+                    style={styles.userPhoto}
+          />
+          <Image
+                     source={{uri: item['teams']['home']['logo']}}
+                    style={styles.userPhoto}
+          />
+        </View>
+        <View
+         style={[
+          styles.box,
+          {
+            flexBasis: 100,
+            flexGrow: 1,
+            flexShrink: 0,
+            //backgroundColor: 'skyblue',
+            justifyContent: "space-around",
+          },
+        ]}>
+         <Text>{item['teams']['away']['name']}</Text>
+         <Text>{item['teams']['home']['name']}</Text>
+        </View>
+        <View style={[
+            styles.box,
+            {
+              flexBasis: 60,
+              flexGrow: 0,
+              flexShrink: 1,
+              //backgroundColor: 'powderblue',
+              justifyContent: "space-around",
+              paddingLeft: 10,
+              //alignItems: 'center',
+            },
+          ]}>
+
+          <Text>{item['scores']['away']['total']} {' '}
+                
+                {!homeWin(item) ? (
+                    <View style={[styles.triangle, styles.arrowLeft]} />
+                  ) : null}
+          </Text>
+          
+          <Text>{item['scores']['home']['total']} {' '}
+                  {homeWin(item) ? (
+                    <View style={[styles.triangle, styles.arrowLeft]} />
+                  ) : null}
+          </Text>
+        </View>
+        <View style={[
+            styles.box,
+            {
+              flexBasis: 1,
+              flexGrow: 0,
+              flexShrink: 1,
+              //backgroundColor: 'steelblue',
+              justifyContent: "space-around",
+              paddingLeft: 0,
+            },
+          ]}>
+          <View style={styles.verticleLine}></View>
+        </View>
+        <View style={[
+            styles.box,
+            {
+              flexBasis: 100,
+              flexGrow: 0,
+              flexShrink: 1,
+              //backgroundColor: 'steelblue',
+              justifyContent: "center",
+              alignSelf: 'center',
+              paddingLeft: 20,
+            },
+          ]}>
+          <Text>{gameStatus(item)}</Text>
+          <Text>{displayDate(item['date'])}</Text>
+        </View>
+        
+        
       </View>
-    );
-  }
-}
+        )}
+        keyExtractor={(item) => item['id']}
+        style={styles.list}
+  />    
+
+ </View>
+    
+
+    
+    
+    
+    
+
+
+      
+
+  );
+}}
 
 const {width} = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
-    margin: 0,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    padding: Configuration.home.listing_item.offset,
+    paddingLeft: 30,
+    paddingRight: 30,
+    paddingTop: 10,
+    flexDirection: 'column',
+  },
+  previewContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: 'aliceblue',
+    marginBottom: 20,
+    borderWidth: 1,
+    borderRadius: 15,
   },
   title: {
     fontWeight: 'bold',
@@ -354,11 +418,17 @@ const styles = StyleSheet.create({
   body: {
     fontSize: 13,
   },
+  box: {
+    flex: 1,
+    height: 100,
+    width: 100,
+  },
   userPhoto: {
     width: 30,
     height: 30,
-    borderRadius: 10,
-    marginRight: 5,
+    alignItems: 'center',
+    //borderRadius: 10,
+    marginLeft: 10,
   },
   flexCol: {
     flexDirection: 'column',
@@ -437,6 +507,9 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     padding: 10,
     elevation: 2,
+    width: 75,
+    alignSelf: 'center',
+    alignItems: 'center',
   },
   buttonOpen: {
     backgroundColor: '#F194FF',
@@ -481,3 +554,4 @@ const styles = StyleSheet.create({
     marginHorizontal: 8,
   },
 });
+
