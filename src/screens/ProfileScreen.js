6e39559470712959b4a1a7e748a0cba5759fb350
Kaylee Tran <kayleetran@g.ucla.edu>
2023-03-13 16:05:54 -0700

    profile page integration, users with any profileID will now have unique profile photos display

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -13,7 +13,14 @@ function ProfileScreen({navigation}) {
   const [balance, setBalance]  = useState(0) 
   const [email, setEmail]  = useState('')
   const [username, setUsername] = useState('')
-  firestore() // check if the user has enough balance
+  const [image, setImage] = useState('')
+
+  const [profilePhotoID, setProfilePhotoID] = useState(1) 
+
+  // const image = require("../../assets/images/hoopProfilePic.png");
+
+
+  firestore() 
       .collection('users')
       .doc(global.currentuid)
       .get()
@@ -22,43 +29,83 @@ function ProfileScreen({navigation}) {
         setUsername(data.fullname)
         setBalance(data.balance)
         setEmail(data.email)
+        setProfilePhotoID(data.profilePhotoNum)
+        if (profilePhotoID ==1 ){
+          setImage (require("../../assets/images/basketballProfilePic.png"))
+        } else if (profilePhotoID ==2){
+          setImage (require ("../../assets/images/hoopProfilePic.png"))
+        } else if (profilePhotoID ==3){
+          setImage (require ("../../assets/images/jerseyProfilePic.png"))
+        }else if (profilePhotoID ==4){
+          setImage (require ("../../assets/images/shoeProfilePic.png"))
+        }else if (profilePhotoID ==5){
+          setImage (require ("../../assets/images/timerProfilePic.png"))
+        }else if (profilePhotoID ==5){
+          setImage (require ("../../assets/images/whistleProfilePic.png"))
+        }
       });
+
+  // function getProfilePhotoID (profilePhotoID) {
+  //   switch (profilePhotoID) {
+  //     case 1:
+  //       setImage (require("../../assets/images/basketballProfilePic.png"))
+  //       break;
+  //     case 2:
+  //       setImage (require ("../../assets/images/hoopProfilePic.png"))
+  //       break;
+  //     case 3:
+  //       setImage (require ("../../assets/images/jerseyProfilePic.png"))
+  //       break;
+  //     case 4:
+  //       setImage (require ("../../assets/images/shoeProfilePic.png"))
+  //       break;
+  //     case 5:
+  //       setImage (require ("../../assets/images/timerProfilePic.png"))
+  //       break;
+  //     case 6:
+  //       setImage (require ("../../assets/images/whistleProfilePic.png"))
+  //       break;
+  //     default:
+  //       setImage (require ("../../assets/icons/profile.png"))
+  //       break;
+  //   }
+  // }
     
-  const removefromfirebase = () => {
-    console.log ("you have reached this function")
-    firestore().collection('users').doc(global.currentuid).delete(
-      {recursive: true, yes: true}
-    );
-    navigation.navigate('LoginStack');
-  }
-  const deleteAccount = () => {
-    Alert.alert('Are you sure?', 'This action cannot be undone.', [
-      {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      {
-        text: 'OK', onPress: () =>  {removefromfirebase()},
-      },
-    ]);
-  }
+  // const removefromfirebase = () => {
+  //   console.log ("you have reached this function")
+  //   firestore().collection('users').doc(global.currentuid).delete(
+  //     {recursive: true, yes: true}
+  //   );
+  //   navigation.navigate('LoginStack');
+  // }
+  // const deleteAccount = () => {
+  //   Alert.alert('Are you sure?', 'This action cannot be undone.', [
+  //     {
+  //       text: 'Cancel',
+  //       onPress: () => console.log('Cancel Pressed'),
+  //       style: 'cancel',
+  //     },
+  //     {
+  //       text: 'OK', onPress: () =>  {removefromfirebase()},
+  //     },
+  //   ]);
+  // }
 
   return (
     <View >
-      <Image style = {styles.userImg} source={AppIcon.images.basketball}/> 
+      <Image style = {styles.userImg} source={image}/> 
       <Text style = {styles.username}> {username} </Text>
       <Text style = {styles.balance}> Your balance: ${balance}</Text>
       <Text style = {styles.fieldTitles}> Email </Text>
       <Text style = {styles.fields} > {email} </Text>
       <Text style = {styles.fieldTitles}> Your referral code: </Text>
       <Text style = {styles.fields} > ur mom 420  </Text>
-      <TouchableOpacity onPress={deleteAccount}>
+      {/* <TouchableOpacity onPress={deleteAccount}>
         <Text style = {{
           textAlign: 'center',
           color: 'red',
         }}> delte acc</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
     </View>
   );
 }
